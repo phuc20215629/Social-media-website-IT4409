73c69d14bb2420773f7f86ae4b368464bac6c19b
Nguyen Dinh Phuc <hanoi2003.a@gmail.com>
2024-06-17 00:05:48 +0700

    test deploy be

--- backend/server.js
+++ backend/server.js
@@ -4,22 +4,20 @@ import connectDB from './db/connectDB.js';
 import cookieParser from 'cookie-parser';
 import userRoutes from './routes/userRoutes.js';
 import postRoutes from './routes/postRoutes.js';
-import messageRoutes from "./routes/messageRoutes.js";
-import {v2 as cloudinary} from "cloudinary";
+import messageRoutes from './routes/messageRoutes.js';
+import { v2 as cloudinary } from 'cloudinary';
 import { app, server } from './socket/socket.js';
 
 dotenv.config(); // to read .env file
 
 connectDB();
 
-
-
 const PORT = process.env.PORT || 6000; // backup the port with 6000 if PORT is not specified in the config
 
 cloudinary.config({
-    cloud_name:process.env.CLOUDINARY_CLOUD_NAME,
-    api_key:process.env.CLOUDINARY_API_KEY,
-    api_secret:process.env.CLOUDINARY_API_SECRET,
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 // Middleware
@@ -30,14 +28,14 @@ app.use(cookieParser()); // to access cookie
 // Routes
 app.use('/api/users', userRoutes);
 app.use('/api/posts', postRoutes);
-app.use("/api/messages", messageRoutes);
+app.use('/api/messages', messageRoutes);
 
-if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '/frontend/dist')));
 
-	// react app
-	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-	});
+    // react app
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'));
+    });
 }
 server.listen(PORT, () => console.log(`server started at http://localhost:${PORT} heyy`));
